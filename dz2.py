64311a8efc0e7d8a0eb365093b3aeba1e82104b3
begimai <begimai095@gmail.com>
2022-10-14 17:11:21 +0600

    rm 1,2dz to day1

--- dz2.py
+++
@@ -1,8 +0,0 @@
-name = 'lenovo'
-mem = '8GB'
-cpu = 'Intel(R) Core(TM) i5'
-ssd = '234G'
-
-print("марка:", name,"\nоперативная память: ", mem)
-print("процессор:", cpu)
-print("жесткий диск:", ssd)
