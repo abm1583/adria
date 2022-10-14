9297d78a6ddc9570ebdb2ba018d2ac7c229b129f
begimai <begimai095@gmail.com>
2022-10-14 15:42:35 +0600

    coffe

---
+++ day1/dz3.py
@@ -0,0 +1,11 @@
+country = 'Korea'
+salary = '9000000000$'
+conditioner= 'yes'
+days = 4
+hours = 7
+
+print('страна:', country)
+print("зарплата:", salary)
+print("кондиционер:", conditioner)
+print("количество рабочих дней:", days)
+print("количество рабочих часов:", hours)
