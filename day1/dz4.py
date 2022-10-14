5bacbea755507dfc7c41e1ce73e3b2a37c644dc6
begimai <begimai095@gmail.com>
2022-10-14 17:09:07 +0600

    lipton

---
+++ day1/dz4.py
@@ -0,0 +1,37 @@
+a = "семья"
+b = "цветы"
+c = "любовь"
+d = "еда"
+e = "обнимашки"
+z = [a, b, c, d, e]
+print("любовь это:", z[0], z[1], z[2],z[3],z[4])
+print('flirt:',z[1],z[3])
+print('milota:', z[4])
+
+cosm = ["илон маск", "ракета", "звезды", "луна", "солнце"]
+print("космос:", cosm)
+
+lamba = "ламба"
+bmw = "бмв"
+kia = "киа"
+honda = "хонда"
+lexux = "лексус"
+f = [lamba, bmw, kia, honda, lexux] 
+print("Марка машин:", f)
+
+vozduh = "воздух"
+gory = "горы"
+trava = "трава"
+reka = "река"
+derevo = "дерево"
+g = [vozduh, gory, trava, reka, derevo]
+print("природа:", g)
+
+
+h = "бог"
+i = "ислам"
+j = "христианство"
+k = "буддизм"
+l = "иудаизм"
+n = [h, i, j, k, l]
+print("религия:", n)
